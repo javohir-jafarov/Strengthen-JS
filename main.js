571889abf72b44f62e3c24bd87e0da7103a4be13
javohir-jafarov <jafarjavohir2010@gmail.com>
2026-02-22 17:02:54 +0500

    complite the filter :)

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let count = 0 
-let up = () => {
-    return count += 1
+let users = [
+    {id: 12, name: "javohir", age: 15, salary: 250, isActive: true},
+    {id: 13, name: "Anvar", age: 12, salary: 100, isActive: true},
+    {id: 14, name: "Sarvar", age: 18, salary: 300, isActive: false},
+    {id: 15, name: "Begzod", age: 21, salary: 200, isActive: false}
+]
+
+let filtered = users.filter(sharabara => sharabara.isActive == true)
+for(let i = 0; i < filtered.length; i++) {
+    console.log(filtered[i].name)
 }
-console.log(up())
-console.log(up())
-console.log(up())
