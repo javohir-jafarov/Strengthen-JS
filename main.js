9aadc287b51c0154436533e8c6272415c8da3a5f
javohir-jafarov <jafarjavohir2010@gmail.com>
2026-02-22 09:46:40 +0500

    learning loops (for, while)

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
-
-const circle = document.createElement('div')
-circle.style.height = '100px'
-circle.style.width = '100px'
-circle.style.backgroundColor = '#00000049'
-circle.style.borderRadius = '50%'
-circle.style.alignItems = 'center'
-circle.style.justifyContent = 'between'
-circle.style.display = 'flex'
-document.body.appendChild(circle)
-circle.className = 'circle'
-
-let lilCircle = document.createElement('div')
-circle.appendChild(lilCircle)
-
-lilCircle.style.height = '50px'
-lilCircle.style.width = '50px'
-lilCircle.style.backgroundColor = '#0000009f'
-lilCircle.style.borderRadius = '50%'
-
-
-window.addEventListener('mousemove', (e) => {
-    circle.style.transform = `translate(calc(${e.clientX}px - 50px), calc(${e.clientY}px - 50px))`,
-    console.log(circle.style.transform)
-})
-
-
-
+for (let i = 0; i <= 100; i++){
+    
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i + "FizzBuzz")
+    } else if (i % 3 == 0) {
+        console.log(i + "Fizz")
+    } else if (i % 5 == 0){
+        console.log(i + "Buzz")
+    }
+}
