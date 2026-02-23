5452f118fc99002a9078eeb314816cc8f959f1e9
javohir-jafarov <jafarjavohir2010@gmail.com>
2026-02-23 22:01:20 +0500

    comlite document and addEventListener ']

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-let users = [
-    {id: 12, name: "javohir", age: 15, salary: 250, isActive: true},
-    {id: 13, name: "Anvar", age: 12, salary: 100, isActive: true},
-    {id: 14, name: "Sarvar", age: 18, salary: 300, isActive: false},
-    {id: 15, name: "Begzod", age: 21, salary: 200, isActive: false}
-]
+let inp = document.querySelector('input')
+let btn = document.querySelector('button')
+let div = document.querySelector('div')
 
-let active = users.filter(item => item.isActive == true)
-for (let i = 0; i < active.length; i++) {
-    console.log(active[i].name)
-}
+let p = document.createElement('p')
 
-let per = users.map(item => item.salary + item.salary * 0.15)
-console.log(per)
+div.appendChild(p)
 
-
-let total = users.reduce((yiguvchi, hozirgi) => yiguvchi + hozirgi.salary, 0)
-console.log("total" + ' ' + total);
+inp.addEventListener('keyup', (e) => {
+    p.innerText = inp.value
+})
